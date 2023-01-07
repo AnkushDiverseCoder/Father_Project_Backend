2ffdefc78c7f5f61f305842bee4fcb9297b318a8
AnkushDiverseCoder <luciferkingofhell1902@gmail.com>
2023-01-07 21:13:46 +0530

    Complete Employee Page Created

--- controllers/EmployeeEnrollmentController.js
+++ controllers/EmployeeEnrollmentController.js
@@ -15,13 +15,7 @@ export const createEmployeeEnrollment = async (req, res, next) => {
 
     const duplicateFind = await EmployeeEnrollment.findOne({
       customerName,
-      joiningDate,
-      enrollmentDate,
-      EmployeeName,
-      fatherName,
       esicIpNumber,
-      Uan,
-      remarks,
     });
 
     if (!duplicateFind) {
@@ -46,6 +40,47 @@ export const createEmployeeEnrollment = async (req, res, next) => {
       status: true,
       msg: "Entry Done Successfully",
     });
+  } catch (error) {
+    res.json({ status: false, msg: error.message });
+  }
+};
+
+
+// IndividualReport
+export const IndividualReportController = async (req, res) => {
+  try {
+    const data = await EmployeeEnrollment.find({
+      customerName: req.body.customerName,
+    });
+
+    res.status(200).json({ status: true, msg: data});
+  } catch (error) {
+    res.json({ status: false, msg: error.message });
+  }
+};
+
+// monthlyReport
+export const monthlyReportController = async (req, res) => {
+  try {
+    const data = await EmployeeEnrollment.find({
+      enrollmentDate: {
+        $gte: new Date(req.body.startDateFormated),
+        $lte: new Date(req.body.endDateFormated),
+      },
+    });
+  
+    res.status(200).json({ status: true, msg: data});
+  } catch (error) {
+    res.json({ status: false, msg: error.message });
+  }
+};
+
+// remove Entry
+export const removeEntry = async (req, res, next) => {
+  try {
+    await EmployeeEnrollment.findByIdAndDelete(req.params.id).then(
+      res.status(200).json({ status: true, msg: "deleted Successfully" })
+    );
   } catch (error) {
     res.json({ status: false, msg: error.message });
   }
--- routes/EmployeeEnrollment.js
+++ routes/EmployeeEnrollment.js
@@ -1,10 +1,19 @@
 import express from "express"
-import { createEmployeeEnrollment} from "../controllers/EmployeeEnrollmentController.js"
+import { IndividualReportController, createEmployeeEnrollment, monthlyReportController, removeEntry} from "../controllers/EmployeeEnrollmentController.js"
 
 const router = express.Router()
 
 //  create routes
 router.post("/", createEmployeeEnrollment)
+
+//  individualReport routes
+router.post("/individualReport", IndividualReportController)
+
+//  historicalReport routes
+router.post("/historicalReport", monthlyReportController)
+
+//  historicalReport routes
+router.delete("/:id", removeEntry)
 
 
 export default router
